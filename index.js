02f9eba21c5e168618ec2ec3010220ca9a074ad1
vickydev777 <vikas.sharma107om@gmail.com>
2024-09-07 13:34:36 +0530

    cors is added in index.js

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const app = express();
+const cors = require('cors');
 var registrationRouter = require('./routes/registration');
 
+app.use(cors());
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin","*");
+	res.header("Access-Control-Allow-Headers","Origins","X-Requested-With","Content-Type","Accept","Authorization");
+	if(req.method === 'OPTIONS'){
+		res.header('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+		return res.status(200).json({});
+	}
 
+});
 
 app.get('/', function (req, res) {
   res.send('Hello World')
